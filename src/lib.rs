eeb527c35abf0ff20a93fd6fff766ff9f2097af9
Patrick Barrett <patrick@psbarrett.com>
2017-07-15 18:48:46 -0500

    add top level doc comment

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+//! `tokio-coap` is a CoAP protocol implementaion
+//! that provides an implementaion of the protocol
+//! for use with`tokio-core`.
+
 extern crate futures;
 extern crate tokio_core;
 extern crate env_logger;
